704d4860724c4c87f789d1a34ad32e9d5a4b07bf
nikita.utkin <nikita.u@aliexpress.ru>
2022-03-16 18:21:28 +0300

    init commit: app is runnable

---
+++ src/test/java/com/interview/authservice/AbstractE2ETest.java
@@ -0,0 +1,33 @@
+package com.interview.authservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Testcontainers
+@DirtiesContext
+public abstract class AbstractE2ETest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Container
+    public static PostgreSQLContainer<?> postgresDB = new PostgreSQLContainer<>("postgres:13.2")
+            .withDatabaseName("auth-service-test");
+
+    @DynamicPropertySource
+    public static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresDB::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresDB::getUsername);
+        registry.add("spring.datasource.password", postgresDB::getPassword);
+    }
+}
---
+++ src/test/java/com/interview/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.interview.authservice;
+
+
+import org.junit.jupiter.api.Test;
+
+public class AuthServiceApplicationTests extends AbstractE2ETest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
